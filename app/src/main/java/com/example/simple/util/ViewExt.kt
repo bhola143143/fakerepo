cd20565024fff65309e41743d09f575528eee46a
bholagupta <bholagupta06@gmail.com>
2022-12-22 17:04:54 +0530

    fake

--- app/src/main/java/com/example/simple/util/ViewExt.kt
+++ app/src/main/java/com/example/simple/util/ViewExt.kt
@@ -16,18 +16,14 @@ import com.example.simple.ScrollChildSwipeRefreshLayout
 
 import com.google.android.material.snackbar.Snackbar
 
-/**
- * Transforms static java function Snackbar.make() to an extension function on View.
- */
+
 fun View.showSnackbar(snackbarText: String, timeLength: Int) {
     Snackbar.make(this, snackbarText, timeLength).run {
         show()
     }
 }
 
-/**
- * Triggers a snackbar message when the value contained by snackbarTaskMessageLiveEvent is modified.
- */
+
 fun View.setupSnackbar(
     lifecycleOwner: LifecycleOwner,
     snackbarEvent: LiveData<Event<Int>>,
